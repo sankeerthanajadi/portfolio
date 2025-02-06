c3fc801506acac6415918128955fcbb0afe4d8c0
sankeerthanajadi <sankeerthanajadi6@gmail.com>
2025-02-06 19:50:36 +0530

    home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,7 +32,7 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarText">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
           <li className="nav-item">
-              <Link className="nav-link active" to="/photo">
+              <Link className="nav-link active" to="/">
                 Home
               </Link>
             </li>
